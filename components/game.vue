9659aaa6aa089021eaaa421f3a9ed31e5ddddc8a
ItzExotical <ItzExotical@gmail.com>
2023-12-19 05:04:00 +0100

    Autosave support

--- components/game.vue
+++ components/game.vue
@@ -1,7 +1,19 @@
 <template>
     <div v-if="!banned" class="bg-dark-primary h-screen items-center justify-center text-center flex flex-col gap-y-10">
+        <UModal v-model="resetopen">
+            <div class="flex flex-col items-center gap-y-4 py-8 text-center">
+                <span class="text-4xl text-gray-100">Reset save data?</span>
+                <span class="text-gray-400">All game progress will be wiped and the game will reload.<br>This can not be undone.</span>
+                <div class="flex gap-x-4">
+                    <UButton @click="resetopen = false" label="Cancel" color="blue" size="lg" class="w-22 h-8 justify-center" trailing-icon="i-lucide-x" />
+                    <UButton @click="Engine.clear()" label="Reset" color="red" size="lg" class="w-22 h-8 justify-center" trailing-icon="i-lucide-trash" />
+                </div>
+            </div>
+        </UModal>
         <span class="text-4xl text-gray-100">Melons: <span class="text-blue-500">{{ melons.toLocaleString() }}</span> (<span class="text-blue-500">{{ mps.toLocaleString() }}</span>/sec)</span>
-        <img draggable=false @click="Engine.handleClick" src="https://cdn.hypefoxstudios.com/data/melon/img/icon.svg" class="w-64 h-64 cursor-pointer" />
+        <button>
+            <img draggable=false @click="Engine.handleClick" src="https://cdn.hypefoxstudios.com/data/melon/img/icon.svg" class="w-64 h-64 cursor-pointer" />
+        </button>
         <span class="text-4xl text-gray-200">Shop for upgrades</span>
         <div class="grid">
             <div v-for="(category, index) in data.buildings.categories" :key="index">
@@ -32,6 +44,9 @@
                 <div>
                     &copy; {{ year }} <NuxtLink to="https://hypefoxstudios.com" class="text-blue-500">Hypefox Studios Ltd</NuxtLink> - All Rights Reserved | App v0.2 | Engine v0.3 | Build 39
                 </div>
+                <div class="pt-4">
+                    <UButton @click="resetopen = true" label="Reset" color="red" size="lg" class="w-22 h-8 justify-center" trailing-icon="i-lucide-trash" />
+                </div>
             </div>
         </footer>
     </div>
@@ -41,17 +56,20 @@
                 <UIcon name="i-lucide-alert-triangle" class="text-red-500 text-[10rem]" />
                 <a class="text-red-500 font-bold text-4xl">{{ "Cheating detected!".toUpperCase() }}</a>
                 <a class="text-red-500 text-xl">Looks like you've been caught red-handed! Remember, in the game of life, cheats never prosper. Better luck next time!</a>
+                <a class="text-red-500 text-xl font-extrabold">Save data has been destroyed.</a>
             </div>
         </div>
     </div>
 </template>
 <script setup lang="ts">
+    import { getData, setData, clear } from "nuxt-storage/local-storage";
     const app = useNuxtApp();
     const settings = app.$settings as any;
     const year = new Date().getFullYear();
     const clickhistory = ref(Array());
     const runtime = ref("00:00:00");
     const data = app.$data as any;
+    const resetopen = ref(false);
     const banned = ref(false);
     const toast = useToast();
     const clicked = ref(0);
@@ -194,6 +212,16 @@
                 };
             };
         };
+        static saveGame() {
+            setData("game", JSON.stringify({
+                melons: melons.value,
+                level: level.value,
+                total: total.value,
+                spent: spent.value,
+                tracking: tracking.value,
+                data: data
+            }));
+        };
         static tick() {
             if (banned.value) {
                 return;
@@ -240,8 +268,19 @@
                 hours++;
             };
             runtime.value = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+            Engine.saveGame();
         };
         static init() {
+            const game = getData("game");
+            if (game) {
+                const parsed = JSON.parse(game);
+                melons.value = parsed.melons;
+                level.value = parsed.level;
+                total.value = parsed.total;
+                spent.value = parsed.spent;
+                tracking.value = parsed.tracking;
+                data.buildings = parsed.data.buildings;
+            };
             lang.value = ticks.value === 1 ? "tick" : "ticks";
             for (const category of data.buildings.categories) {
                 for (const building of category.members) {
@@ -262,6 +301,15 @@
             melons.value++;
             clicked.value++;
             Engine.anticheat();
+            if (!banned.value) {
+                Engine.saveGame();
+            } else {
+                clear("game");
+            };
+        };
+        static clear() {
+            clear("game");
+            location.reload();
         };
         static anticheat() {
             if (clickhistory.value.length >= 11) {
